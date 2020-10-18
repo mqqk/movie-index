cc4084da12064bf3a2294d62b3815f40cc433f69
Mick Styers <68083989+mqqk@users.noreply.github.com>
2020-10-18 16:50:21 -0400

    production mode imminent

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const helmet = require('helmet')
 const MOVIEDEX = require('./moviedex.json')
 
 const app=express();
-app.use(morgan('dev'))
+const morganSettings  = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSettings))
 app.use(helmet())
 app.use(cors())
 
@@ -42,8 +43,18 @@ app.get('/movie', function handleSortMovie(req,res) {
     res.json(response)
 })
 
-const PORT=8001
+app.use((error,req,res,next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response- {error: {message:'server error'}}
+    } else {
+        response = {error}
+    }
+    res.status(500).json(response)
+})
+
+const PORT= process.env.PORT || 8001
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+    // console.log(`Server listening at http://localhost:${PORT}`)
 })
